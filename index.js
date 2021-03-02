d950819475da158f211c45025a60c760e433019e
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2021-03-02 07:36:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -11,20 +10,18 @@ const app = express();
 
 dotenv.config();
 
-
-
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/questions', questionRoutes);
 
+const CONNECTION_URL = process.env.CONNSTRING;
+const PORT = process.env.PORT || 5000;
 
-const CONNECTION_URL = process.env.REACT_APP_DBCONSTRING;
-const PORT = process.env.PORT|| 5000;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+mongoose
+	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+	.catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
